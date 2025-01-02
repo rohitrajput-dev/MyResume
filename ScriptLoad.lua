e765ac814df9a4402f128b89b523764db7d025e4
Rohit Rajput <108916412+rohitrajput-dev@users.noreply.github.com>
2025-01-02 21:33:50 +0530

    Update ScriptLoad.lua

--- ScriptLoad.lua
+++ ScriptLoad.lua
@@ -8,7 +8,7 @@ local list = {
 	[994732206] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/rohitrajput-dev/MyResume/refs/heads/main/BloxFruitTestOnly.lua'))()"
 }
 _G.HoHoLoaded = true
-_G.MY_KEY_IS = "57c20f8b1e63bcbeb25927a729360dece86aa42d50b565e74e082ee7ff71bf52"
+--_G.MY_KEY_IS = "57c20f8b1e63bcbeb25927a729360dece86aa42d50b565e74e082ee7ff71bf52"
 notify.New("Wait Bro!", 30)
 
 if list[game.GameId] ~= nil then
